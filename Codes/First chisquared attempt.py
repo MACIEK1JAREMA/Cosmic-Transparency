ae53489cb3866661489044c3aef5b3c078bee0aa
MACIEK1JAREMA <87180511+MACIEK1JAREMA@users.noreply.github.com>
2022-02-17 14:54:05 +0000

    First updates to chi^2
    Improved the chi^2 code a little. Reduced unneeded operations, plotted the result more cleanly. Plotted our found model and model with Om=0.23 on data. Made it general for different accuracies, and used accuracy that our convergence study flagged at having error < 0.2%.
    Will continue now, will try to optimise it somewhat if I can.

--- Codes/First chisquared attempt.py
+++ Codes/First chisquared attempt.py
@@ -1,9 +1,77 @@
+'''
+Here we proceed with first attempts of chi^2 analysis of the data and
+previously devleoped model, treating only Omega_{m} (Om) as a free parameter.
+'''
 
 # import modules
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
 #%%
+
+# Read in data as pandas dataframe
+df = pd.read_excel('data\\SNe data.xlsx')
+df = df.sort_values('z')  # increasing z sort
+
+# define constants
+H0 = 70*10**3  # taking 70km s^-1 Mpc^-1
+c = 3 * 10**8
+OL = 0.77
+
+# set up the model axis
+Om = np.linspace(0, 1, 90)
+z = np.linspace(0, 1.8, 100)
+count = np.linspace(0, len(z)-1, len(z)).astype(int)
+count = list(count)
+
+z10 = np.linspace(0, 1.8, 3000)  # inetrgal approximation axis
+count10 = list(np.linspace(0, len(z10)-1, len(z10)).astype(int))
+
+# set up figure and visuals for chi^2 plot
+fig = plt.figure()
+ax = fig.gca()
+ax.set_xlabel(r'$\Omega_{m} $', fontsize=16)
+ax.set_ylabel(r'$\chi^2$', fontsize=16)
+
+i = 0
+chisq_array = np.array([])
+
+while i < len(Om):
+    # model from list comprehension
+    dl1_sum = [(c/H0) * np.sum(1/np.sqrt(Om[i]*(1+z10[:int(len(z10)/len(z))*j + 1])**3 + OL)) for j in count[:]]
+    dl1_model = (1+z)*z/(count10[::int(len(z10)/len(z))]) * dl1_sum
+    dl1mu_model = 5*np.log10(dl1_model) + 25
+
+    interp = np.interp(df['z'], z, dl1mu_model)  # interpolate the values in the grid as they are generated
+    
+    # get chi^2 value for this Om and save to its array
+    chisq = np.sum(((interp - df['mu'])/(df['dmu']))**2)
+    chisq_array = np.append(chisq_array, chisq)
+    
+    i += 1
+
+# plot chi^2 against Omega_m
+ax.plot(Om, chisq_array)
+
+# get minimum value for Om and chi^2
+index = chisq_array.argmin()
+min_Om = Om[index]
+min_chisq = chisq_array[index]
+
+print('minimising chisquared gives a matter density of ', min_Om)
+
+# %%
+
+# develop the model that corresponds to the found Om, and plot it o data
+# also plot the one when using Om = 0.23
+
+# Plotting with data together:
+
+# define constants
+H0 = 70*10**3  # taking 70km s^-1 Mpc^-1
+c = 3 * 10**8
+OL = 0.77
 
 # Read in data as pandas dataframe
 df = pd.read_excel('data\\SNe data.xlsx')
@@ -11,48 +79,47 @@ df = pd.read_excel('data\\SNe data.xlsx')
 # sort it in increasing z:
 df = df.sort_values('z')
 
-# define constants
-H0 = 70*10**3  # taking 70km s^-1 Mpc^-1
-c = 3 * 10**8
-Om = np.linspace(0,1,90)
-OL = 0.77
+# convert the mu data to d_L, make it a new column and sort w.r.t it
+df_dL = 10**(0.2*df['mu'] - 5)
+df.insert(4, 'dL Mpc', df_dL)
+df = df.sort_values('dL Mpc')
 
-# axis
-z = np.linspace(0, 1.8, 100)  # defining 100
+# propagate errors and add as new column to data frame
+ddL = 0.2*np.log(10)*10**(0.2*df['mu'] - 5) * df['dmu']
+df.insert(5, 'ddL Mpc', ddL)
+
+# set up figure and visuals
+fig = plt.figure()
+ax = fig.gca()
+ax.set_xlabel(r'$Redshift \ z$', fontsize=16)
+ax.set_ylabel(r'$Luminosity \ Distance  \ d_{L} \  [Mpc]$', fontsize=16)
+
+# plot the data as errorbar plot
+ax.errorbar(df['z'], df['dL Mpc'], yerr=df['ddL Mpc'],
+            capsize=2, fmt='.', markersize=5, ecolor='k')
+
+# Calculate models:
+
+# set up axis
+z = np.linspace(0, 1.8, 100)  # for model
 count = np.linspace(0, len(z)-1, len(z)).astype(int)
 count = list(count)
-z10 = np.linspace(0, 1.8, 1000)  # using 1000 point for sum
+z10 = np.linspace(0, 1.8, 1000)  # for integral approximation
 count10 = list(np.linspace(0, len(z10)-1, len(z10)).astype(int))
+# develop model
+dl1_sum = [(c/H0) * np.sum(1/np.sqrt(min_Om*(1+z10[:int(len(z10)/len(z))*j + 1])**3 + OL)) for j in count[:]]
+dl1_model = (1+z)*z/(count10[::int(len(z10)/len(z))]) * dl1_sum
 
-dl1_grid = np.zeros([len(z),len(Om)])
-#%%
-
-i = 0
-chisq_array = np.array([])
-while i < len(Om):
-
-    # model from list comprehension 
-    dl1_sum = [(c/H0) * np.sum(1/np.sqrt(Om[i]*(1+z10[:10*j + 1])**3 + OL)) for j in count[:]]
-    dl1_model = (1+z)*z/(count10[::10]) * dl1_sum
-    dl1mu_model = np.array([5*np.log10(dl1_model) + 25])
-
-    
-    dl1_grid[:, i] = dl1mu_model #calculate grid of different possible model mu values
-    interp = np.interp(df['z'], z, dl1_grid[:, i]) #interpolate the values in the grid as they are generated
-    chisq = np.sum((interp-df['mu'])**2/(df['dmu']**2)) # calculate chisq for each dataset
-    
-    chisq_array = np.append(chisq_array, chisq) #create array of chisq of each Om
-    
-    
-    i+=1
-    
-plt.plot(Om, chisq_array)
-
-#min value at the following
-
-index = chisq_array.argmin()
-min_Om = Om[24]
-min_chisq = chisq_array[24]
+# plot above models, with a legend:
+ax.plot(z, dl1_model, 'g-', label=rf'$Model \ with \ \chi^2 \ minimised \ \Omega_m \ = \ {round(min_Om, 4)}$')
 
 
-print('minimising chisquared gives a matter density of ', min_Om)
+# plot with Om = 0.23
+dl1_sum = [(c/H0) * np.sum(1/np.sqrt(0.23*(1+z10[:int(len(z10)/len(z))*j + 1])**3 + OL)) for j in count[:]]
+dl1_model = (1+z)*z/(count10[::int(len(z10)/len(z))]) * dl1_sum
+
+# plot above models, with a legend:
+ax.plot(z, dl1_model, 'r-', label=rf'$Model \ with \ \Omega_m \ = \ 0.23$')
+
+ax.legend()
+
