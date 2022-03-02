c5c405204b53bc419c4b84f292332269dcf6133a
ALonelylepton <billsmith34124@gmail.com>
2022-03-02 14:51:45 +0000

    Update Gaussian Likelihods.py

--- Codes/multiparameter - initial/Gaussian Likelihods.py
+++ Codes/multiparameter - initial/Gaussian Likelihods.py
@@ -12,6 +12,17 @@ import matplotlib.cm as cm
 from mpl_toolkits.mplot3d import Axes3D
 import scipy.stats as st
 
+def integrate(F, x_min, x_max):
+    value_strip = 0
+    value_total = 0
+    width = (x_max - x_min)/np.size(F[0,:])
+    i = 0
+    for i in range(np.size(F[0,:])):
+        value_strip = width*F[i]
+        value_total += value_strip
+    return value_total
+
+
 # %%
 
 # Read in Sne data as pandas dataframe
@@ -37,6 +48,48 @@ chisq_array -= np.min(chisq_array)  # define min chi^2 to be 0
 #defining Gaussian
 g = np.exp(-(H0/1000-73)**2/2)/np.sqrt(2*np.pi)
 
+
+# switch to likelihoods
+likelihood = np.exp((-chisq_array**2)/2)
+
+#multipying likelihood by Gaussian
+weighted = np.zeros(np.shape(likelihood))
+i=0
+while i < len(g):
+    weighted[:,i] = likelihood[:,i]*g[i]
+    i+=1
+
+#marginalising over H0
+lik_margin = np.trapz(weighted, x = H0, axis = 1) #integrating along columns(along H0)
+
+#trying maciej's integration formula (NEED TO FINISH THIS!!!!)
+#test = integrate(weighted, H0[0], H0[-1])
+
+#normalising
+lik_margin /= np.sum(lik_margin)
+
+Om_found = Om[np.where(lik_margin == np.max(lik_margin))[0]]
+
+# find peak value and where 68.3% of it lies for 1 \sigma error
+variables = st.rv_discrete(values=(Om, lik_margin))
+confidence1 = variables.interval(0.683)[1] - Om_found
+confidence2 = Om_found - variables.interval(0.683)[0]
+
+print(f'Om = {round(Om_found[0], 5)}')
+print('\n')
+print(f'with confidence: \n')
+print(f'                +{round(confidence1[0], 6)}')
+print(f'                -{round(confidence2[0], 6)}')
+
+fig = plt.figure()
+ax = fig.gca()
+ax.plot(Om[60:90], lik_margin[60:90])
+ax.set_ylabel(r'$Likelihood \ marginalised \ over \ H_{0} \ L(\Omega_{m})$', fontsize=16)
+ax.set_xlabel(r'$\Omega_{m}$', fontsize=16)
+ax.set_title('$Marginalised \ Gaussian \ Likelihood \ Function$', fontsize=16)
+
+#%% plotting stuff
+
 #plotting Gaussian
 fig = plt.figure()
 ax = fig.gca()
@@ -45,10 +98,19 @@ ax.set_ylabel('$g(H_0)$', fontsize=16)
 ax.set_xlabel('$H_0 \ (km s^{-1} Mpc^{-1})$', fontsize=16)
 ax.set_title('$First \ Gaussian \ Prior$', fontsize=16)
 
-# switch to likelihoods
-likelihood = np.exp((-chisq_array**2)/2)
-weighted = np.zeros(np.shape(likelihood))
-i=0
-while i < len(g):
-    weighted[:,i] = likelihood[:,i]*g[i]
-    i+=1
+# plot in 3D, but restrict range:
+fig2 = plt.figure()
+ax2 = fig2.gca(projection='3d')
+ax2.set_ylim(0.2, 0.3)
+Hgrid, Omgrid = np.meshgrid(H0, Om[60:90])
+weighted = weighted[60:90, :]
+surf = ax2.plot_surface(Hgrid/1000, Omgrid, weighted, cmap=cm.jet)
+ax2.set_ylabel('$\Omega_{m} $', fontsize=16)
+ax2.set_xlabel('$H_0 \ (km s^{-1} Mpc^{-1})$', fontsize=16)
+ax2.set_zlabel('likelihood', fontsize=16)
+
+
+
+
+
+
