95c77d43c09b1f7ab3b3e4b429598d45dfd63e35
MACIEK1JAREMA <87180511+MACIEK1JAREMA@users.noreply.github.com>
2022-03-06 16:15:14 +0000

    Error study in new confidences.

--- Codes/multiparameter - initial/dev. confidence function.py
+++ Codes/multiparameter - initial/dev. confidence function.py
@@ -56,7 +56,7 @@ def integrate2D(F, xg, yg, indx_1x=0, indx_2x=-1, indx_1y=0, indx_2y=-1, interp=
     return result
 
 
-def confidence(F, xg, yg, accu, interp, interp_kind='linear'):
+def confidence(F, xg, yg, accu, interp=0, interp_kind='linear'):
     '''
     Find heights corresponding to confidence levels of a 2D function:
     
@@ -79,6 +79,8 @@ def confidence(F, xg, yg, accu, interp, interp_kind='linear'):
         for contours to be in ascending order for matplotlib.
     
     '''
+    
+    global integrals, heights
     
     # interpolate F if user wanted to:
     if interp > 10:
@@ -106,33 +108,34 @@ def confidence(F, xg, yg, accu, interp, interp_kind='linear'):
     
     # 1-sigma:
     integrals = np.array(integrals)
-    s1_indx = np.where(np.diff(np.sign(integrals - 0.683)))[0]
+    s1_indx = np.where(np.diff(np.sign(integrals - 0.6826)))[0]
     s1_height = heights[s1_indx][0]
     
     # 2-sigma:
     integrals = np.array(integrals)
-    s2_indx = np.where(np.diff(np.sign(integrals - 0.955)))[0]
+    s2_indx = np.where(np.diff(np.sign(integrals - 0.9546)))[0]
     s2_height = heights[s2_indx][0]
     
     # 3-sigma:
     integrals = np.array(integrals)
-    s3_indx = np.where(np.diff(np.sign(integrals - 0.997)))[0]
+    s3_indx = np.where(np.diff(np.sign(integrals - 0.9973)))[0]
     s3_height = heights[s3_indx][0]
     
     return [s3_height, s2_height, s1_height]
 
+# %%
 
 # test the above
 if __name__ == '__main__':
     # define 2D grids
-    v = np.linspace(0, 1, 100)
+    v = np.linspace(0, 1, 1000)
     xg, yg = np.meshgrid(v, v)
     
     # define a 2D unnormalised gaussian:
     F = xg*yg**2
     
     # normalise it to Volume = 1: with our integrate 2D fucntion:
-    norm = integrate2D(F, xg, yg, interp=1000)
+    norm = integrate2D(F, xg, yg)
     F *= 1/norm
     
     # check that it's right by analytical result
@@ -141,7 +144,7 @@ if __name__ == '__main__':
     print(f'Our intgeration fucntion gave: {np.round(norm, 4)}')
     
     # give it to the confidence fucntion to get sigma regions:
-    heights = confidence(F, xg, yg, accu=1000, interp=1000)
+    levels = confidence(F, xg, yg, accu=1000)
     
     # plot these as contours on a conour map
     fig = plt.figure()
@@ -150,7 +153,7 @@ if __name__ == '__main__':
     ax.set_xlabel(r'$x$', fontsize=20)
     ax.set_ylabel(r'$y$', fontsize=20)
     heatmap = ax.pcolormesh(xg, yg, F)
-    contourplot = ax.contour(xg, yg, F, heights, cmap=cm.jet)
+    contourplot = ax.contour(xg, yg, F, levels, cmap=cm.jet)
     ax.clabel(contourplot, fontsize=16)
     fig.colorbar(heatmap)
 
@@ -198,7 +201,83 @@ if __name__ == '__main__':
     
     ax1.legend(fontsize=20)
 
+# %%
 
+# extra convergence study of confidence heights and their time to run
+
+import time
+
+if __name__ == '__main__':
+    
+    # set up h accuracies to use:
+    accus = np.arange(100, 3000, 300)
+    values = []  # list to store results for 1 sigma
+    times = []  # list to save time to run for each
+    
+    for a in accus:
+        
+        start = time.perf_counter()
+        
+        # define 2D grids
+        v = np.linspace(0, 1, 1000)
+        xg, yg = np.meshgrid(v, v)
+        
+        # define a 2D unnormalised gaussian:
+        F = xg*yg**2
+        
+        # normalise it to Volume = 1: with our integrate 2D fucntion:
+        norm = integrate2D(F, xg, yg)
+        F *= 1/norm
+        
+        # give it to the confidence fucntion to get sigma regions: and save 1sigma one
+        levels = confidence(F, xg, yg, accu=a)
+        values.append(levels[2])
+        
+        # save time to run
+        end = time.perf_counter()
+        times.append(end-start)
+        
+        # inform progress:
+        print(f'done run with accuracy = {a}')
+    
+    # find relative errors
+    values = np.array(values)
+    change = abs((np.diff(values) / values[:-1])*100)
+    
+    # plot them:
+    fig1 = plt.figure()
+    ax1 = fig1.gca()
+    ax1.tick_params(labelsize=16)
+    ax1.set_xlabel(r'$height \ accuracy$', fontsize=20)
+    ax1.set_ylabel(r'$relative \ error \ [\%]$', fontsize=20)
+    ax1.plot(accus[1:], change)
+    
+    # plot horizontal levels for reference
+    ax1.axhline(1, color='r', ls='-.', label=r'$1\%$')
+    ax1.axhline(0.5, color='g', ls='-.', label=r'$0.5\%$')
+    ax1.axhline(0.1, color='b', ls='-.', label=r'$0.1\%$')
+    ax1.axhline(0.01, color='orange', ls='-.', label=r'$0.01\%$')
+    
+    ax1.legend(fontsize=20)
+    
+    # plot the last one as contours on a conour map
+    fig2 = plt.figure()
+    ax2 = fig2.gca()
+    ax2.tick_params(labelsize=16)
+    ax2.set_xlabel(r'$x$', fontsize=20)
+    ax2.set_ylabel(r'$y$', fontsize=20)
+    heatmap = ax2.pcolormesh(xg, yg, F)
+    contourplot = ax2.contour(xg, yg, F, levels, cmap=cm.jet)
+    ax2.clabel(contourplot, fontsize=16)
+    fig2.colorbar(heatmap)
+    
+    # plot the time to run for each
+    fig3 = plt.figure()
+    ax3 = fig3.gca()
+    ax3.tick_params(labelsize=16)
+    ax3.set_xlabel(r'$height \ accuracy$', fontsize=20)
+    ax3.set_ylabel(r'$time \ to \ run \ [s]$', fontsize=20)
+    ax3.plot(accus, times)
 
 
 
