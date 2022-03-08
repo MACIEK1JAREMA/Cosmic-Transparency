Proposed fixes to chi^2 calculation
All detailed in Diary as usual

Codes/Module.py
@@ -141,17 +141,22 @@ def chi_confidence1D(y, x, axis, colors=['r', 'g', 'b'], labels=True, interp=100
         -- interp - int, if > 10, we interpolate grids and F to that size,
                  default = 0
     Returns:
-        None, plots on given axis
+        1 sigma x values as list in format [-error, +error]
     '''
     
-    
-    # add plot of convergence:
+    # interpolate if user wished to
     if interp > 10:
         y = np.interp(np.linspace(x[0], x[-1], interp), x, y)
         x = np.linspace(x[0], x[-1], interp)
-        indx1 = np.argwhere(np.diff(np.sign(y - np.ones(np.shape(y)))))
-        indx2 = np.argwhere(np.diff(np.sign(y - 2.71*np.ones(np.shape(y)))))
-        indx3 = np.argwhere(np.diff(np.sign(y - 9*np.ones(np.shape(y)))))
+    
+    # get indexes of sigma level intersections
+    indx1 = np.argwhere(np.diff(np.sign(y - np.ones(np.shape(y)))))
+    indx2 = np.argwhere(np.diff(np.sign(y - 2.71*np.ones(np.shape(y)))))
+    indx3 = np.argwhere(np.diff(np.sign(y - 9*np.ones(np.shape(y)))))
+    
+    # get minimum and so mean x measurement:
+    indx_min = np.where(y == np.min(y))
+    x_mean = x[indx_min]
         
     # get values of intersections
     x1 = x[indx1]
@@ -184,6 +189,9 @@ def chi_confidence1D(y, x, axis, colors=['r', 'g', 'b'], labels=True, interp=100
         axis.legend()
     else:
         pass
+    
+    # return values of 1 sigma regions
+    return x_mean[0] - x1[0], x1[1] - x_mean[0]
 
 
 # example of chi_confidence1D use:

Codes/first models/chisquared attempt - keep k=0.py
@@ -68,11 +68,12 @@ c = 3 * 10**8
 # set up the model axis
 Om = np.linspace(0, 1, 300)
 z = np.linspace(np.min(df['z']), 1.8, 100)
+#z = np.linspace(0, 1.8, 100)
 count = np.linspace(0, len(z)-1, len(z)).astype(int)
 count = list(count)
 
 z10 = np.linspace(0, 1.8, 1000)  # inetrgal approximation axis
-count10 = np.linspace(0, len(z10)-1, len(z10)).astype(int)+1
+count10 = np.linspace(0, len(z10)-1, len(z10)).astype(int) + 1
 
 i = 0
 chisq_array = np.array([])
@@ -105,15 +106,21 @@ min_chisq = chisq_array[index]
 # plotting in the relevant bounds with confidence regions
 Delta_squared = 20
 
+fig = plt.figure()
+ax = fig.gca()
+ax.set_xlabel(r'$\Omega_{m} $', fontsize=20)
+ax.set_ylabel(r'$\chi^2$', fontsize=20)
+ax.plot(Om, chisq_array)
+
 chisq_array -= np.min(chisq_array)  # define min chi^2 to be 0
 in_index = np.where(chisq_array <= Delta_squared)
 chisq_array = chisq_array[in_index]  # only keep in wanted region
 Om = Om[in_index]  # crop Om accordingly
 
-fig = plt.figure()
-ax1 = fig.gca()
-ax1.set_xlabel(r'$\Omega_{m} $', fontsize=16)
-ax1.set_ylabel(r'$\chi^2$', fontsize=16)
+fig1 = plt.figure()
+ax1 = fig1.gca()
+ax1.set_xlabel(r'$\Omega_{m} $', fontsize=20)
+ax1.set_ylabel(r'$\chi^2$', fontsize=20)
 
 ax1.set_ylim(0, 20)
 
@@ -186,7 +193,7 @@ ax1.errorbar(df['z'], df['dL Mpc'], yerr=df['ddL Mpc'],
 # Calculate models:
 
 # set up axis
-z = np.linspace(0, 1.8, 100)  # for model
+z = np.linspace(np.min(df['z']), 1.8, 100)  # for model
 count = np.linspace(0, len(z)-1, len(z)).astype(int)
 count = list(count)
 z10 = np.linspace(0, 1.8, 1000)  # for integral approximation

Codes/first models/convergence study 1.py
@@ -49,6 +49,10 @@ for accu in accuracy:
 
 # Models sampled at 4 z values, relative errors plotted for each
 
+# Read in data as pandas dataframe
+df = pd.read_excel('data\\SNe data.xlsx')
+df = df.sort_values('z')  # sort it in increasing z
+
 # define constants
 H0 = 70*10**3  # taking 70km s^-1 Mpc^-1
 c = 3 * 10**8
@@ -69,21 +73,21 @@ ax3.set_ylabel(r'$relative \ error\  [\%] $', fontsize=16)
 # Calculate the model for a rnage of accuracies in z10, saving a few chosen dL
 
 # set up needed arrays
-z = np.linspace(0, 1.8, 100)  # defining 100
+z = np.linspace(np.min(df['z']), 1.8, 100)  # defining 100
 count = np.linspace(0, len(z)-1, len(z)).astype(int)
 count = list(count)
 
 # set up an array of accuracies and empty lists to store 5 smapled dL
-accuracy = np.arange(100, 2001, 100)
+accuracy = np.arange(100, 1000, 100)
 dL_sampled = np.empty((4, len(accuracy)))
-z_i = [1, 32, 67, 77]  # indexes of z at which we save dL
+z_i = [2, 32, 67, 77]  # indexes of z at which we save dL
 num = len(z_i)
 i = 0  # counting variable for indexes from accu
 
 # loop over calculating the whole model for a few accuracies:
 for accu in accuracy:
     z10 = np.linspace(0, 1.8, accu)  # using 1000 point for sum
-    count10 = list(np.linspace(0, accu-1, accu).astype(int))
+    count10 = np.linspace(0, accu-1, accu).astype(int) + 1
     dl1_sum = [(c/H0) * np.sum(1/np.sqrt(Om*(1+z10[:int(accu/len(z))*j + 1])**3 + OL)) for j in count[:]]
     dl1_model = (1+z)*z/(count10[::int(accu/len(z))]) * dl1_sum
     # add sampled points to saving array:
@@ -99,12 +103,6 @@ for i in range(num):
 # #############################################################################
 # for each z_i, find average error in the data points at z nearby
 # #############################################################################
-
-# Read in data as pandas dataframe
-df = pd.read_excel('data\\SNe data.xlsx')
-
-# sort it in increasing z:
-df = df.sort_values('z')
 
 # convert the mu data to d_L, make it a new column and sort w.r.t it
 df_dL = 10**(0.2*df['mu'] - 5)

Codes/first models/first likelihood L(Om).py
@@ -22,12 +22,12 @@ c = 3 * 10**8
 
 # set up the model axis
 Om = np.linspace(0, 1, 500)
-z = np.linspace(0, 1.8, 100)
+z = np.linspace(np.min(df['z']), 1.8, 100)
 count = np.linspace(0, len(z)-1, len(z)).astype(int)
 count = list(count)
 
 z10 = np.linspace(0, 1.8, 1000)  # inetrgal approximation axis
-count10 = list(np.linspace(0, len(z10)-1, len(z10)).astype(int))
+count10 = np.linspace(0, len(z10)-1, len(z10)).astype(int) + 1
 
 i = 0
 chisq_array = np.array([])

Codes/multiparameter - initial/Gaussian Likelihods.py
@@ -55,7 +55,7 @@ c = 3 * 10**8
 # set up the model axis
 H0 = np.linspace(70, 76, 300)*10**3
 Om = np.linspace(0, 1, 300)
-z = np.linspace(0, 1.8, 100)
+z = np.linspace(np.min(df['z']), 1.8, 100)
 
 # finding minimum of chisquared coords
 chisq_array -= np.min(chisq_array)  # define min chi^2 to be 0
@@ -154,7 +154,7 @@ c = 3 * 10**8
 # set up the model axis
 H0 = np.linspace(70, 76, 300)*10**3
 Om = np.linspace(0, 1, 300)
-z = np.linspace(0, 1.8, 100)
+z = np.linspace(np.min(df['z']), 1.8, 100)
 
 # finding minimum of chisquared coords
 chisq_array -= np.min(chisq_array)  # define min chi^2 to be 0
@@ -245,7 +245,7 @@ c = 3 * 10**8
 # set up the model axis
 H0 = np.linspace(70, 76, 300)*10**3
 Om = np.linspace(0, 1, 300)
-z = np.linspace(0, 1.8, 100)
+z = np.linspace(np.min(df['z']), 1.8, 100)
 
 # finding minimum of chisquared coords
 chisq_array -= np.min(chisq_array)  # define min chi^2 to be 0

Codes/multiparameter - initial/first likelihoods - with H0.py
@@ -19,7 +19,7 @@ df = pd.read_excel('data\\SNe data.xlsx')
 df = df.sort_values('z')  # increasing z sort
 
 # Read in generated array
-chisq_df = pd.read_excel('data\\Chisquare_array(60-80).xlsx')
+chisq_df = pd.read_excel('data\\(60-80) redone.xlsx')
 chisq_array_init = np.array(chisq_df)
 chisq_array = chisq_array_init[:, 1:]
 
@@ -29,7 +29,7 @@ c = 3 * 10**8
 # set up the model axis
 H0 = np.linspace(60, 80, 300)*10**3
 Om = np.linspace(0, 1, 300)
-z = np.linspace(0, 1.8, 100)
+z = np.linspace(np.min(df['z']), 1.8, 100)
 
 # finding minimum of chisquared coords
 chisq_array -= np.min(chisq_array)  # define min chi^2 to be 0
@@ -71,7 +71,7 @@ df = pd.read_excel('data\\SNe data.xlsx')
 df = df.sort_values('z')  # increasing z sort
 
 # Read in generated array
-chisq_df = pd.read_excel('data\\Chisquare_array(70-76).xlsx')
+chisq_df = pd.read_excel('data\\(70-76) redone.xlsx')
 chisq_array_init = np.array(chisq_df)
 chisq_array = chisq_array_init[:, 1:]
 
@@ -81,7 +81,7 @@ c = 3 * 10**8
 # set up the model axis
 H0 = np.linspace(70, 76, 300)*10**3
 Om = np.linspace(0, 1, 300)
-z = np.linspace(0, 1.8, 100)
+z = np.linspace(np.min(df['z']), 1.8, 100)
 
 # finding minimum of chisquared coords
 chisq_array -= np.min(chisq_array)  # define min chi^2 to be 0
@@ -126,7 +126,7 @@ chisq_array = chisq_array_init[:, 1:]
 # set up the model axis
 H0 = np.linspace(70, 76, 300)*10**3
 Om = np.linspace(0, 1, 300)
-z = np.linspace(0, 1.8, 100)
+z = np.linspace(np.min(df['z']), 1.8, 100)
 c = 3 * 10**8  # light speed
 
 # finding minimum of chisquared coords
@@ -183,7 +183,7 @@ c = 3 * 10**8
 # set up the model axis
 H0 = np.linspace(70, 76, 300)*10**3
 Om = np.linspace(0, 1, 300)
-z = np.linspace(0, 1.8, 100)
+z = np.linspace(np.min(df['z']), 1.8, 100)
 
 # finding minimum of chisquared coords
 chisq_array -= np.min(chisq_array)  # define min chi^2 to be 0

Codes/multiparameter - initial/implicit marginalising over H0.py
@@ -77,7 +77,8 @@ c = 3 * 10**8
 
 # set up the model axis
 Om = np.linspace(0, 1, 300)
-z = np.linspace(0, 1.8, 100)
+#z = np.linspace(0, 1.8, 100)
+z = np.linspace(np.min(df['z']), 1.8, 100)
 count = np.linspace(0, len(z)-1, len(z)).astype(int)
 count = list(count)
 
@@ -93,6 +94,8 @@ while i < len(Om):
     # model from list comprehension
     dl_sum = [(c/H0) * np.sum(1/np.sqrt(Om[i]*(1 + z10[:int(len(z10)/len(z))*j + 1])**3 - Om[i] + 1)) for j in count[:]]
     dl_model = (1+z)*z/(count10[::int(len(z10)/len(z))]) * dl_sum
+    
+    #dl_model[0] = dl_model[1] - (dl_model[2] - dl_model[1])
     
     # interpolate the values to match data size
     dl_model_interp = np.interp(x=df['z'], xp=z, fp=dl_model)
@@ -159,10 +162,9 @@ print('the starndard deviation was found to be:')
 print(f' + {Omi[indx1[1]] - Om[np.where(chisq_array == np.min(chisq_array))[0][0]]}')
 print(f' - {Om[np.where(chisq_array == np.min(chisq_array))[0][0]] - Omi[indx1[0]]}')
 
-#%%
-#Plotting default chisquared for different M to illustrate dependence (can delete later)
+# %%
 
-
+# Plotting default chisquared for different M to illustrate dependence (can delete later)
 
 # Read in data as pandas dataframe
 df = pd.read_excel('data\\SNe data.xlsx')
@@ -217,35 +219,3 @@ while k < len(M):
 
 plt.title('$\chi^2 \ of \ model \ with \ H_0=70 \ km s^{-1} Mpc^{-1}$')
 ax1.legend()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

Codes/multiparameter - initial/varying H0 initial comparisons.py
@@ -106,7 +106,7 @@ ax2.set_ylim(0, 20)
 
 # set up the model axis
 Om = np.linspace(0, 1, 300)
-z = np.linspace(0, 1.8, 100)
+z = np.linspace(np.min(df['z']), 1.8, 100)
 count = np.linspace(0, len(z)-1, len(z)).astype(int)
 count = list(count)
 z10 = np.linspace(0, 1.8, 1000)  # inetrgal approximation axis