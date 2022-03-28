fe2dd98804794d542eae5610a29d5984741bc6ec
ALonelylepton <billsmith34124@gmail.com>
2022-03-28 03:03:48 +0100

    Varying H0 for new H(z) model then including it as a parameter

--- Codes/Hz/marginalising H0.py
+++ Codes/Hz/marginalising H0.py
@@ -14,3 +14,71 @@ import time
 
 # %%
 
+start = time.perf_counter()
+
+# Read in data as pandas dataframe
+df = pd.read_excel('data\\Hz\\Wei Hz data.xlsx')
+df = df.sort_values('z')  # increasing z sort
+
+# define constants
+H0 = np.linspace(60,80, 500)
+
+# set up the model axis
+Om = np.linspace(0, 0.6, 1000)
+z = np.linspace(np.min(df['z']), np.max(df['z']), 2000)
+
+# loop over models for all Om
+j = 0
+chisq_array = np.zeros((len(Om),len(H0)))
+while j < len(H0):
+    i=0
+    while i < len(Om):
+        # develop model from equation as for E(z)
+        model = H0[j] * np.sqrt(Om[i]*(1+z)**3 - Om[i] + 1)
+        
+        # interpolate the values to match data size
+        model_interp = np.interp(x=df['z'], xp=z, fp=model)
+        
+        # get chi^2 value for this Om
+        chisq = np.sum(((model_interp - df['Hz'])/(df['dHz']))**2)
+        
+        # save to array and update index
+        chisq_array[i,j] = chisq
+        i += 1
+    
+    print(j)
+    j+=1
+
+end = time.perf_counter()
+print(f'time to run: {end - start}')
+#%%
+# to save results: (make sure to change names each time it's run)
+
+dataframe = pd.DataFrame(chisq_array)
+
+# writing to Excel
+datatoexcel = pd.ExcelWriter('2D chisquared for H(z).xlsx')
+
+# write DataFrame to excel
+dataframe.to_excel(datatoexcel)
+
+# save the excel
+datatoexcel.save()
+print('DataFrame is written to Excel File successfully.')
+
+
+
+
+#%%
+
+Hgrid, Omgrid = np.meshgrid(H0,Om)
+
+chisq_array -= np.min(chisq_array)
+
+fig = plt.figure()
+ax=fig.gca()
+heatmap = ax.pcolormesh(Hgrid, Omgrid, chisq_array)
+contourplot = ax.contour(Hgrid, Omgrid, chisq_array, np.array([2.30, 4.61, 11.8]), cmap=cm.jet)
+fig.colorbar(heatmap)
+ax.set_ylabel(r'$\Omega_{m}$', fontsize=16)
+ax.set_xlabel(r'$H_0 \ km \ s^{-1} \ Mpc^{-1}$', fontsize=16)
