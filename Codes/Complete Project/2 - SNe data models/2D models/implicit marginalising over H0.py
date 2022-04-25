4160ee203f9a1e0ff91a5fe519f639a45447a1e0
MACIEK1JAREMA <87180511+MACIEK1JAREMA@users.noreply.github.com>
2022-04-25 13:33:36 +0100

    Figure cosmetics

--- Codes/Complete Project/2 - SNe data models/2D models/implicit marginalising over H0.py
+++ Codes/Complete Project/2 - SNe data models/2D models/implicit marginalising over H0.py
@@ -10,7 +10,7 @@ import Codes.Module as module
 # %%
 
 # Read in Sne data as pandas dataframe
-df = pd.read_excel('data\\SNe data.xlsx')
+df = pd.read_excel('Codes\\Complete Project\\Datasets\\SNe data.xlsx')
 df = df.sort_values('z')  # increasing z sort
 
 # define constants
@@ -88,7 +88,7 @@ ax2.tick_params(labelsize=16)
 ax2.set_xlabel(r'$\Omega_{m}$', fontsize=20)
 ax2.set_ylabel(r'$\chi^2$', fontsize=20)
 ax2.set_ylim(0, 20)
-ax2.plot(Om, chisq_array)
+ax2.plot(Om, chisq_array, 'k')
 
 # add plot of confidence regions:
 lerror, rerror = module.chi_confidence1D(chisq_array, Om, ax2)
@@ -103,68 +103,10 @@ print(f' - {np.round(rerror, 5)}')
 
 # %%
 
-# Plotting default chisquared for different M to illustrate dependence (can delete later)
-
-# Read in data as pandas dataframe
-df = pd.read_excel('data\\SNe data.xlsx')
-df = df.sort_values('z')  # increasing z sort
-
-# define constants
-H0 = 70*10**3  # taking 70km s^-1 Mpc^-1
-c = 3 * 10**8
-
-# set up the model axis
-Om = np.linspace(0, 1, 300)
-z = np.linspace(0, 1.8, 100)
-count = np.linspace(0, len(z)-1, len(z)).astype(int)
-count = list(count)
-
-z10 = np.linspace(0, 1.8, 1000)  # inetrgal approximation axis
-count10 = list(np.linspace(0, len(z10)-1, len(z10)).astype(int))
-
-i = 0
-k = 0
-chisq_array = np.array([])
-M = np.array([24.5,24.9,25.1,25.5])
-
-
-fig = plt.figure()
-ax1 = fig.gca()
-ax1.set_xlabel(r'$\Omega_{m} $', fontsize=16)
-ax1.set_ylabel(r'$\chi^2$', fontsize=16)
-test = M[3]
-while k < len(M):
-    while i < len(Om):
-        # model from list comprehension
-        dl1_sum = [(c/H0) * np.sum(1/np.sqrt(Om[i]*(1 + z10[:int(len(z10)/len(z))*j + 1])**3 - Om[i] + 1)) for j in count[:]]
-        dl1_model = (1+z)*z/(count10[::int(len(z10)/len(z))]) * dl1_sum
-        # convert to mu vs z to compare to data.
-        dl1mu_model = 5*np.log10(dl1_model) + M[k]
-        
-        # interpolate the values in the grid as they are generated
-        interp = np.interp(df['z'], z, dl1mu_model)
-        
-        # get chi^2 value for this Om and save to its array
-        chisq = np.sum(((interp - df['mu'])/(df['dmu']))**2)
-        chisq_array = np.append(chisq_array, chisq)
-        
-        i += 1
-    
-    plt.plot(Om, chisq_array, label=fr'$M = {round(M[k], 4)}$')
-    print(M[k])
-    k = k + 1
-    i=0
-    chisq_array = np.array([])
-
-plt.title('$\chi^2 \ of \ model \ with \ H_0=70 \ km s^{-1} Mpc^{-1}$')
-ax1.legend()
-
-# %%
-
-# Testing ideas
+# Checking M_th
 
 # Read in Sne data as pandas dataframe
-df = pd.read_excel('data\\SNe data.xlsx')
+df = pd.read_excel('Codes\\Complete Project\\Datasets\\SNe data.xlsx')
 df = df.sort_values('z')  # increasing z sort
 
 # define constants
@@ -225,21 +167,17 @@ ax1.set_ylabel(r'$\chi^2$', fontsize=16)
 ax1.set_ylim(0, 20)
 ax1.plot(Om, chisq_array)
 
-# add plot of confidence:
-Omi = np.linspace(0, 1, 10000)
-chi_sqr_i = np.interp(np.linspace(0, 1, 10000), Om, chisq_array)
-indx1 = np.argwhere(np.diff(np.sign(chi_sqr_i - np.ones(np.shape(chi_sqr_i)))))
-indx2 = np.argwhere(np.diff(np.sign(chi_sqr_i - 2.71*np.ones(np.shape(chi_sqr_i)))))
-indx3 = np.argwhere(np.diff(np.sign(chi_sqr_i - 9*np.ones(np.shape(chi_sqr_i)))))
-confidence_plot(Omi, chi_sqr_i, indx1, indx2, indx3, ax1)
+
+# add plot of confidence regions:
+lerror, rerror = module.chi_confidence1D(chisq_array, Om, ax1)
 
 print('\n')
 print('minimum value was found to be at Omega_m = ')
 print(Om[np.where(chisq_array == np.min(chisq_array))[0][0]])
 print('\n')
 print('the starndard deviation was found to be:')
-print(f' + {Omi[indx1[1]] - Om[np.where(chisq_array == np.min(chisq_array))[0][0]]}')
-print(f' - {Om[np.where(chisq_array == np.min(chisq_array))[0][0]] - Omi[indx1[0]]}')
+print(f' + {np.round(lerror, 5)}')
+print(f' - {np.round(rerror, 5)}')
 
 # get variation in M_th:
 Mth_mean = np.mean(Mth)
